0a5a6295dcaa5faf695e208e737f9dfb269ee329
Виктор <viktor.bozhilov@gmail.com>
2016-08-29 08:37:06 +0300

    some code for uploading files

--- views/users/profile.php
+++ views/users/profile.php
@@ -3,7 +3,7 @@
 $this->heading = str_replace(" - Кухнята на Мони","",$this->title);
 ?>
 <h1><?= htmlspecialchars($this->heading) ?></h1>
-<form method="post">
+<form method="post" enctype="multipart/form-data">
     <div>Потребителско име:</div>
     <input type="text" name="username" value="<?=$_SESSION['username']?>" disabled><br>
     <div>Парола:</div>
@@ -16,6 +16,8 @@ $this->heading = str_replace(" - Кухнята на Мони","",$this->title);
     <input type="text" name="email" value="<?=htmlspecialchars($this->user['email'])?>"><br>
     <div>Телефон:</div>
     <input type="text" name="phone" value="<?=htmlspecialchars($this->user['phone'])?>"><br>
+    <div>Профилна снимка:</div>
+    <input type="file" name="fileToUpload" id="fileToUpload">
     <div>
         <input type="submit" value="Редактирай">
     </div>
